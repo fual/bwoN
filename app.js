58a94edd6c2e922b2b0de3149eb5a911d8f9ea7f
fual <f20music@narod.ru>
2015-04-03 01:13:02 +0300

    Поправил проверку логина.

--- app.js
+++ app.js
@@ -4,70 +4,93 @@
 var express = require('express'),
     app = express(),
     stylus = require('stylus'),
-    nib = require('nib');
+    nib = require('nib'),
+    md5 = require('MD5'),
+    bodyParser = require('body-parser');
 
-var bodyParser = require('body-parser');
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+//Firebase
+var Firebase = require("firebase");
+var fb = new Firebase("https://pppsss.firebaseio.com");
+/***********************
+ *
+ * Express Settings
+ *
+ **********************/
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-var Firebase = require("firebase");
-var fb = new Firebase("https://pppsss.firebaseio.com");
+/***********************
+ *
+ * Express Settings
+ *
+ **********************/
 function compile(str, path) {
     return stylus(str)
         .set('filename', path)
         .use(nib());
 }
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
 
+
+/***********************
+ *
+ * Css Generation
+ *
+ **********************/
 app.use(stylus.middleware(
-    { src: __dirname + '/public'
+    {
+        src: __dirname + '/public'
         , compile: compile
     }
 ));
 
 app.use(express.static(__dirname + '/public'));
 
+
+/***********************
+ *
+ * Page Generation
+ *
+ **********************/
 app.get('/', function (req, res) {
-    /*fb.set({
-        'user' : {
-            'mail' : {
-                'alex@mangust' : '2001',
-                'alex2@mangust' : '2002',
-                'alex3@mangust' : '2003'
-            },
-            'password':{
-                '2001' : '123',
-                '2002' : '123',
-                '2003' : '123'
-            }
-        }
-    });*/
-    //fb.child('user').push({"name":"alex3","password":"300"});
-    //fb.child('user').push({"name":"alex3","password":"500"});
-
-
     res.render('index',
-        { title : 'Home'}
+        {title: 'Home'}
     )
 });
 
+/***********************
+ *
+ * Auth
+ *
+ **********************/
 app.post('/auth', function (req, res) {
-    var email = req.body.mail;
-    var state;
-    new Firebase('https://pppsss.firebaseio.com/user/mail/').child('alex2@mangsust').once('value', function(snap) {
-        if(snap.val()==null){
-            //Добавить новый пароль
-            state = 'newCreate'
-        } else {
-            state = 'сверить логины'
-        }
-    });
-    res.end();
-    console.log(req.body)
+    var login = req.body.userLogin,
+        password = md5(req.body.userPas),
+        state = '',
+        response = null;
+    if (login && password) {
+        fb.child('users').orderByChild('login').equalTo(login).on("value", function (snapshot) {
+            response = snapshot.val();
+        });
 
+        if (response == null) {
+            fb.child('users').push({"login": login, "password": password});
+            state = 'newCreate';
+        } else {
+            console.log(response);
+            console.log( password + ' = ' + response['password'] );
+            if (password == response.password) {
+                state = 'success Login';
+            } else {
+                state = 'that login is using!';
+            }
+        }
+    }
+
+    res.send(state);
 });
 
 app.listen(5000, function () {
